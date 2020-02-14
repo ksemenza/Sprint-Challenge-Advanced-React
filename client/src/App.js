b206d0fda1b12a42c5e1ae130391015bec971def
unknown <ksemenza13@gmail.com>
2020-02-14 14:04:51 -0500

    completed MVP and some stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import trophy from './img/trophy.svg'
+import {render, getByLabelText, getByTestId} from '@testing-library/react'
 import Players from './components/Players'
-import logo from './logo.svg';
+import DarkMode from "./hooks/DarkMode"
 import './App.css';
 
 function App() {
+
+  const [darkMode, setDarkMode] = DarkMode(false)
+
+  const toggleDarkMode = e => {
+    e.preventDefault()
+    setDarkMode(!darkMode)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+
+      <header className="App-header"><h1>Women's World Cup Soccer</h1>
+       <img src={trophy} className="trophy" alt="trophy" />
+ <button data-testid='dark-btn' onClick={toggleDarkMode}>Dark Mode</button>
+ </header>
+ <div>
  <Players/>
-        <a
+  </div>
+  <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -17,7 +33,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
